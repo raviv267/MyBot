247f5b17b4e771f5b223ffe5ef8cee2dbb9e77f7
raviv267 <36600744+raviv267@users.noreply.github.com>
2018-03-28 12:57:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,8 @@ import textwrap
 import inspect
 from contextlib import redirect_stdout
 from discord.ext import commands
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),description="= Command List = \n\nHelp Commands:",owner_id=231028316843278346)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),description="= Command List = \n\n
+[Use .help <commandname> for details]",owner_id=231028316843278346)
 bot.load_extension("cogs.developer")
 
 

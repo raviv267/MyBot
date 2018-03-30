2e43dacd66037d582e85a8d953f0a53eda48a4f5
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-30 10:57:03 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),description="= Command List ⚠️ = \n\nUse .help <commandname> for deteails:",owner_id=231028316843278346)
 bot.load_extension("cogs.developer")
 bot.load_extension("cogs.mod")
+bot.load_extension("cogs.help")
 
 def dev_check(id):
     if id == 277981712989028353 or id == 231028316843278346:
@@ -89,84 +90,6 @@ async def invite(ctx):
     await ctx.send("Yay! thanks for adding me! -> https://discordapp.com/oauth2/authorize?client_id=428310138890223646&scope=bot&permissions=8")
 
 
-@bot.command()
-async def hack(ctx, user: discord.Member):
-    """Hack someone's account!"""
-    msg = await ctx.send(f"Currently hacking: **{user}**")
-    await asyncio.sleep(2)
-    await msg.edit(content="Please wait while we grab the information... [▓▓                  ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓                ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓▓▓              ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓▓▓▓▓            ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓▓▓▓▓▓▓          ]")
-    await asyncio.sleep(3)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓▓▓▓▓▓▓▓▓        ]")
-    await asyncio.sleep(3)
-    await msg.edit(content="Please wait while we grab the information... [▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ]")
-    await asyncio.sleep(4)
-    await msg.edit(content=f"An error has occurred while hacking **{user}**. Please try again later. ❌") 
-
-    
-@bot.command()
-async def roast(ctx, user: discord.Member = None):
-    '''Roast someone! If you suck at roasting them yourself.'''
-    lol = f"Hey, {user.mention}! " if user is not None else ""
-    roasts = ["I'd give you a nasty look but you've already got one.", "If you're going to be two-faced, at least make one of them pretty.", "The only way you'll ever get laid is if you crawl up a chicken's ass and wait.", "It looks like your face caught fire and someone tried to put it out with a hammer.", "I'd like to see things from your point of view, but I can't seem to get my head that far up your ass.", "Scientists say the universe is made up of neutrons, protons and electrons. They forgot to mention morons.", "Why is it acceptable for you to be an idiot but not for me to point it out?", "Just because you have one doesn't mean you need to act like one.", "Someday you'll go far... and I hope you stay there.", "Which sexual position produces the ugliest children? Ask your mother.", "No, those pants don't make you look fatter - how could they?", "Save your breath - you'll need it to blow up your date.", "If you really want to know about mistakes, you should ask your parents.", "Whatever kind of look you were going for, you missed.", "Hey, you have something on your chin... no, the 3rd one down.", "I don't know what makes you so stupid, but it really works.", "You are proof that evolution can go in reverse.", "Brains aren't everything. In your case they're nothing.", "I thought of you today. It reminded me to take the garbage out.", "You're so ugly when you look in the mirror, your reflection looks away.", "Quick - check your face! I just found your nose in my business.", "It's better to let someone think you're stupid than open your mouth and prove it.", "You're such a beautiful, intelligent, wonderful person. Oh I'm sorry, I thought we were having a lying competition.", "I'd slap you but I don't want to make your face look any better.", "You have the right to remain silent because whatever you say will probably be stupid anyway."]
-    await ctx.send(f"{lol}{random.choice(roasts)}")
-    
-
-@bot.command()
-async def dogmeme(ctx):
-    """Make a dog meme with this awesome command!"""
-    one = await ctx.send("Please enter the text you want on the top half of the meme.")
-    try:
-        x = await bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
-    except asyncio.TimeoutError:
-        return await ctx.send("Request timed out. Please try again.")
-    two = await ctx.send("Great! Now enter the text you want on the bottom half of the meme.")
-    try:
-        f = await bot.wait_for("message", check=lambda f: f.channel == ctx.channel and f.author == ctx.author, timeout=60.0)
-    except asyncio.TimeoutError:
-        return await ctx.send("Request timed out. Please try again.")
-    await ctx.message.delete()
-    await one.delete()
-    await x.delete()
-    await two.delete()
-    await f.delete()
-    em = discord.Embed(color=discord.Color(value=0x00ff00), title="Done my magic! Here's your dank meme.")
-    em.set_image(url=f"https://memegen.link/doge/{x.content.replace(' ', '_')}/{f.content.replace(' ', '_')}.jpg")
-    await ctx.send(embed=em)
-
-    
-@bot.command()
-async def insult(ctx, user: discord.Member = None):
-    """You insult me i insult you!"""
-    try:
-        await ctx.message.delete()
-    except discord.Forbidden:
-        pass
-    if user is None:
-        await ctx.send("Who are you insulting? Me? :thinking:")
-    else:
-        insults = ['fuck you', 'go suck my dick', 'suck my dick', 'fucking gay', 'you little bitch', 'can you fuck off already?', 'fu, smd', 'hope you die']
-        await ctx.send(f"{user.mention}, {random.choice(insults)}")
-        
-    
-@bot.command()
-async def kill(ctx, user: discord.Member = None):
-    """Kill someone. DIE!"""
-    if user is None:
-        msg = await ctx.send("Didn't enter anyone, so guess I'm coming for you. :dagger:")
-        await asyncio.sleep(3)
-        await msg.edit(content=f"Swoooooosh. **{ctx.author.name}** died. :skull_crossbones:")
-    else:
-        msg = await ctx.send(f"{user.mention}, I'm coming for you. :dagger:")
-        await asyncio.sleep(3)
-        await msg.edit(content=f"Swoooooosh. **{user.name}** died to **{ctx.author.name}**. :skull_crossbones:")
 
         
 @bot.command(aliases=['8ball'])

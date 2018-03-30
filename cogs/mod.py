6d45b1abd298c540955b92cd26466adc18f86088
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-30 09:37:19 -0400

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -18,7 +18,7 @@ class Moderator:
     async def kick(self, ctx, user: discord.Member = None):
         """Kicks a member. Gotta hurt."""
         if user is None:
-            await ctx.send("To boot the member, use the command like this: \n*kick [@user] [reason]")
+            await ctx.send("To boot the member, use the command like this: \n*kick [@user]")
         try:
             await user.kick()
             messages = ['it was about time.', 'take the L.', 'get outta here.', 'get booted!', 'nice try.']
@@ -32,12 +32,35 @@ class Moderator:
     async def ban(self, ctx, user: discord.Member = None):
         """Swings the mighty Ban Hammer on that bad boy."""
         if user is None:
-            await ctx.send("To swing the ban hammer, use the command like this: \n*ban [@user] [days of msgs to delete] [reason]")
+            await ctx.send("To swing the ban hammer, use the command like this: \n*ban [@user]")
         try:
             await user.ban()
             await ctx.send("BONG! The ban hammer fell on {user.name}.")
         except discord.Forbidden:
             await ctx.send("RIP. I don't have permissions to ban!")
+
+            
+    
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def mute(self, ctx, user: discord.Member):
+        '''Forces someone to shut up. Usage: *mute [user] [time in mins]'''
+        try:
+            await ctx.channel.set_permissions(user, send_messages=False)
+            await ctx.send(f"**{user.name}**, it's time to shut up.")
+        except discord.Forbidden:
+            await ctx.send("Watch my permissions! I don't have enough.")
+            
+            
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unmute(self, ctx, user: discord.Member):
+        '''Allows someone to un-shut up. Usage: *unmute [user]'''
+        try:
+            await ctx.channel.set_permissions(user, send_messages=True)
+            await ctx.channel.send(f"{user.mention} is now un-shutted up.")
+        except discord.Forbidden:
+            await ctx.send("Couldn't unmute the user. Uh-oh...")
             
             
 def setup(bot): 
